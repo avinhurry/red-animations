109c172bbcaf7cd9eedf39d05f13cf4e4811b1cd
avinhurry <avinhurry@icloud.com>
2021-12-23 16:47:00 +0000

    Add coming soon page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root "about#index"
+  root "coming_soon#index"
 
   get "/about", to: "about#index"
+  get "/coming-soon", to: "coming_soon#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
